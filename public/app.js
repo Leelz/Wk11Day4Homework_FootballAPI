ff8ccf94281450809121b75bfc9946f55f1a4380
Leelz <adam.leel93@gmail.com>
2017-01-28 18:01:39 +0000

    football api fun

--- public/app.js
+++ public/app.js
@@ -1,61 +1,47 @@
-var albums = null;
+var app = function(){
+  var url = 'http://api.football-data.org/v1/competitions/';
+  bindEvents();
+  makeRequest("GET", url, requestComplete);
+}
 
-var makeRequest = function (url, callback) {
+var bindEvents = function() {
+  var searchQuery = document.getElementById('search-query');
+  var competitionsDiv = document.getElementById('compList');
+
+  searchQuery.onkeyup = function() {
+  var jsonString = this.responseText;
+  var competitions = JSON.parse(jsonString);
+    competitionsDiv.innerHTML = '';
+    var url = 'http://api.football-data.org/v1/competitions/' + competitions.id + '/leagueTable'
+    makeRequest("GET", url, requestComplete);
+  }
+}
+
+var makeRequest = function (method, url, callback) {
   var request = new XMLHttpRequest();
   request.open('GET', url);
   request.onload = callback;
   request.send();
 }
 
-var handleJSON = function(albums){
-  var input = document.querySelector('#search-query');
-  var inputValue = input.value;
-  var container = document.querySelector('#albums');
-  for (album of albums) {
-    var albumName = document.createElement('p')
-    albumName.innerText = album.name;
-    container.appendChild(albumName)
+var display = function(competitions){
+  var container = document.getElementById('compList');
+
+  for (competition of competitions) {
+    var div = document.createElement('div');
+    div.className = "competition";
+    div.innerHTML = "<a href='https://www.google.co.uk/search?q= '>"  + competition.caption + "</a>";
+    container.appendChild(div)
   }
 }
-
-// var searchcontent = function () {
-
-// }
 
 var requestComplete = function () {
   if (this.status !== 200) return;
   var jsonString = this.responseText;
-  var albums = JSON.parse(jsonString);
-  console.log(albums);
-  albums = albums.albums.items;
-  console.log(albums);
-  handleJSON(albums);
+  var competitions = JSON.parse(jsonString);
+  // var competitionsList = competitions;
+  // console.log(competitions)
+  display(competitions);
 };
-
-var handleButtonClick = function (event) {
-  var buttonResult = document.querySelector('#search-query');
-  var searchResult = document.querySelector('#albums')
-  buttonResult.innerText = 'Woah! I totally just got clicked ' + 
-  counter + ' times';
-  console.log(event);
-}
-
-var app = function(){
-  var url = 'https://api.spotify.com/v1/search?q=christmas&type=album';
-  makeRequest(url, requestComplete);
-}
-
-var setAlbumDisplay = function(album) {
-  var info = document.querySelector('#albums');
-  info.innerText = "";
-  var name = document.createElement("p");
-  info.appendChild(name);
-  name.innerText = "Name: " + album.name;
-
-  // save(country);
-  var button = document.querySelector('button'); 
-  button.onclick = handleButtonClick;
-}
-
 
   window.onload = app;
